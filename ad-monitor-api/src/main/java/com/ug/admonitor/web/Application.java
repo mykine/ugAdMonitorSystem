9dc9a694154338dcd338124579ed6879e088aa22
mykine <mykine@sina.com>
2021-05-23 23:29:55 +0800

    使用默认的HikariDataSource数据源连接池操作sql

--- ad-monitor-api/src/main/java/com/ug/admonitor/web/Application.java
+++ ad-monitor-api/src/main/java/com/ug/admonitor/web/Application.java
@@ -12,14 +12,14 @@ public class Application {
         //1.返回IOC容器
        ConfigurableApplicationContext ioc = SpringApplication.run(Application.class,args);
 
-        //2.查看容器中的组件bean名字
-        String[] names = ioc.getBeanDefinitionNames();
-        for (String name :names
-             ) {
-            System.out.println("name="+name);
-        }
-
-        Cat littleCat1 = ioc.getBean("littleCat",Cat.class);
-        System.out.println("name="+littleCat1.getName());
+//        //2.查看容器中的组件bean名字
+//        String[] names = ioc.getBeanDefinitionNames();
+//        for (String name :names
+//             ) {
+//            System.out.println("name="+name);
+//        }
+//
+//        Cat littleCat1 = ioc.getBean("littleCat",Cat.class);
+//        System.out.println("name="+littleCat1.getName());
     }
 }
---
+++ ad-monitor-api/src/test/java/com/ug/admonitor/web/ApplicationTest.java
@@ -0,0 +1,27 @@
+package com.ug.admonitor.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Slf4j
+@SpringBootTest
+public class ApplicationTest {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Test
+    void sqlTest(){
+        Long count = jdbcTemplate.queryForObject("select count(id) from t_users ",Long.class);
+        log.info("count={}",count);
+    }
+
+    @Test
+    void sqlTest2(){
+        Long count = 999L;
+        log.info("count={}",count);
+    }
+}
